03131027dda7d7049e9237e799111a85a913f97a
Brennan Gamwell <brennan@segment.com>
2019-12-04 14:53:12 -0800

    Initial commit.

---
+++ .github/actions/add-tag/index.js
@@ -0,0 +1,28 @@
+const github = require('@actions/github')
+const core = require('@actions/core')
+
+const {
+    GITHUB_TOKEN
+} = process.env
+const PR_LABEL = 'TEST'
+const octokit = new github.GitHub(GITHUB_TOKEN)
+
+async function run () {
+  const { number, pull_request } = github.context.payload
+
+    // No need to check if the label has already been applied.
+  if (pull_request.labels.includes(PR_LABEL)) {
+    return
+  }
+
+  await octokit.issues.addLabels({
+    issue_number: number,
+    labels: [PR_LABEL]
+  })
+}
+
+try {
+  run()
+} catch (err) {
+  core.fail(err)
+}
